3b7363cff3adb1a30c2ae582b7d1c2f38f285305
elisammg <elisammg@outlook.com2git config --global user.email elisammg@outlook.com>
2018-02-18 22:08:35 -0500

    merge

---
+++ Tarea 3/ConsoleApp1/ConsoleApp2/Lista.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public abstract class Lista
+    {
+
+        interface IList<T>
+        {
+            public abstract void
+                
+
+        }   
+             
+
+        
+    }
+
+          
+}
+   
---
+++ Tarea 3/ConsoleApp1/XUnitTestProject1/ListTest.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+
+namespace ListTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Setup
+            
+
+            // Acto
+            
+
+            // Verificacion
+           
+        }
+    }
+}
